8ca09c66eeeba5bbfcd38582cf8f348128bf529f
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2024-09-18 05:17:06 -0300

    Initial Commit

---
+++ VeganSuperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeganSuperApp
+//
+//  Created by Rodrigo Cerqueira Reis on 18/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
